e541e91e75a59dabc92dbe765cf563c99143e349
nicocadq <nmachado.camejo@gmail.com>
2020-08-19 13:54:01 -0300

    Temporal commit

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useDispatch } from "react-redux";
-import { deleteColumn } from "../../slicers/column";
+import { useDispatch, useSelector } from "react-redux";
+// import { deleteColumn } from "../../slicers/column";
+import { addCard } from "../../slicers/card";
+
+import styles from "./Column.module.scss";
 
 const Column = ({ id }) => {
   const dispatch = useDispatch();
+  const title = "asdasdasd";
+  const cards =
+    useSelector((state) =>
+      Object.values(state.cards).filter((card) => card.columnID === id)
+    ) || [];
 
   const handleClick = () => {
-    dispatch(deleteColumn(id));
+    dispatch(addCard(id));
   };
 
-  return <div onClick={handleClick}>{id}</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h3>{title}</h3>
+        </div>
+        <div className={styles.cards}>
+          {cards.map((card) => (
+            <div>{card.id}</div>
+          ))}
+        </div>
+        <div className={styles["add-new-card"]} onClick={handleClick}>
+          Add new Card
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Column.propTypes = {
---
+++ src/slicers/card.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "nanoid";
+
+const cardSlice = createSlice({
+  name: "cards",
+  initialState: {},
+  reducers: {
+    addCard(state, action) {
+      const newCard = {
+        id: nanoid(),
+        columnID: action.payload,
+        text: "asdasdasdasdasad",
+      };
+      state[newCard.id] = newCard;
+    },
+    deleteCard(state, action) {
+      const key = action.payload;
+      delete state[key];
+    },
+  },
+});
+
+export const { addCard, deleteCard } = cardSlice.actions;
+export default cardSlice.reducer;
