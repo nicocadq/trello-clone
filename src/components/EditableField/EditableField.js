7fdbdfb058febd5fd7fc7190ad17b408e04d98e2
nicocadq <nmachado.camejo@gmail.com>
2020-08-20 13:33:49 -0300

    Changed EditableField to make that don't update the sotre

--- src/components/EditableField/EditableField.js
+++ src/components/EditableField/EditableField.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { useDispatch } from "react-redux";
 
 const EditableField = ({ actionOnSave, textDefault }) => {
   const [text, setText] = useState("");
   const [active, setActive] = useState(!textDefault);
-  const dispatch = useDispatch();
 
   const handleOnChange = (e) => {
     setText(e.target.value);
@@ -14,7 +12,7 @@ const EditableField = ({ actionOnSave, textDefault }) => {
   const handleOnBlur = () => {
     setActive(false);
     if (text.length > 0) {
-      dispatch(actionOnSave(text));
+      actionOnSave(text);
       setText("");
     }
   };
@@ -28,6 +26,10 @@ const EditableField = ({ actionOnSave, textDefault }) => {
           onChange={handleOnChange}
           value={text}
           autoFocus
+          style={{
+            padding: "2px",
+            width: "100%",
+          }}
         />
       ) : (
         <p onClick={() => setActive(true)}> {textDefault || text} </p>
